04fddca18fa68a90a93918ab9f3bdf8872ce39ab
qjd-yyds <qjd1774@163.com>
2022-07-20 22:57:37 +0800

    feat: 学习redux第一天

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button, List } from 'antd';
 export default function Home() {
+  const [list, setList] = useState([
+    {
+      text: '测试'
+    }
+  ]);
   return (
     <div>
-      <h1>home</h1>
-
+      <Input style={{ width: '300px' }}></Input>
+      <Button>提交</Button>
+      <div style={{ width: '300px' }}>
+        <List bordered dataSource={list} renderItem={item => <List.Item>{item.text}</List.Item>} />
+      </div>
     </div>
   );
 }

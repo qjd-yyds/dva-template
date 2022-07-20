57b062cafec979012ba489f9b58e3c07ccec1cc7
qjd-yyds <qjd1774@163.com>
2022-07-20 23:09:02 +0800

    feat: 建立store文件

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import store from '~/store';
 import { Input, Button, List } from 'antd';
 export default function Home() {
-  const [list, setList] = useState([
-    {
-      text: '测试'
-    }
-  ]);
+  const state = store.getState()
   return (
     <div>
-      <Input style={{ width: '300px' }}></Input>
+      <Input placeholder={state.defaultVale} style={{ width: '300px' }}></Input>
       <Button>提交</Button>
       <div style={{ width: '300px' }}>
-        <List bordered dataSource={list} renderItem={item => <List.Item>{item.text}</List.Item>} />
+        <List bordered dataSource={state.list} renderItem={item => <List.Item>{item.text}</List.Item>} />
       </div>
     </div>
   );
---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+  defaultVale: '写点什么东西',
+  list: [
+    {
+      text: '测试'
+    }
+  ]
+};
+export default (state = defaultState, action) => {
+  return state;
+};

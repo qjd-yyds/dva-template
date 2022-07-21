e8cdb765783546304f5835e992e0004db02cb95c
qjd-yyds <qjd1774@163.com>
2022-07-21 22:51:25 +0800

    feat: 学习redux第二天

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import store from '~/store';
 import { Input, Button, List } from 'antd';
 export default function Home() {
-  const state = store.getState()
+  const state = store.getState();
+  const [num, setNum] = useState(1);
+  const storeChange = () => {
+    setNum(num + 1);
+  };
+  const submit = () => {
+    store.dispatch({ type: 'ADD' });
+  };
+  store.subscribe(storeChange);
+
+  const handleInput = e => {
+    const action = {
+      type: 'changeInput',
+      value: e.target.value
+    };
+    store.dispatch(action);
+  };
   return (
     <div>
-      <Input placeholder={state.defaultVale} style={{ width: '300px' }}></Input>
-      <Button>提交</Button>
+      <Input value={state.defaultValue} onChange={handleInput} style={{ width: '300px' }}></Input>
+      <Button onClick={submit}>提交</Button>
       <div style={{ width: '300px' }}>
         <List bordered dataSource={state.list} renderItem={item => <List.Item>{item.text}</List.Item>} />
       </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 const defaultState = {
-  defaultVale: '写点什么东西',
+  defaultValue: '写点什么东西',
   list: [
     {
       text: '测试'
@@ -7,5 +7,19 @@ const defaultState = {
   ]
 };
 export default (state = defaultState, action) => {
+  switch (action.type) {
+    case 'changeInput':
+      return {
+        ...state,
+        defaultValue: action.value
+      };
+    case 'ADD':
+      const value = state.defaultValue;
+      return {
+        ...state,
+        defaultValue: '',
+        list: [...state.list, { text: value }]
+      };
+  }
   return state;
 };
